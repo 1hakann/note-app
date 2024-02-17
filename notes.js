127ea79bd4a77f5b25ce39dfa70e9e5a4b73b8aa
Hakan Çakmak <hakan.cakmak@live.com>
2024-02-17 15:22:10 +0300

    note app v-01

--- notes.js
+++ notes.js
@@ -9,13 +9,12 @@ const addNote = (title, body) => {
     const notes = loadNotes();
     const duplicateNote = notes.find((note) => note.title === title)
 
-
     if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
         })
-        saveNotes(notes)
+        savedNotes(notes)
         console.log(chalk.green.inverse('New note added!'))
     } else {
         console.log(chalk.red.inverse('Note title taken!'))
